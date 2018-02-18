c81340018d1faec83d4b4991aea4a9f2df11e348
Warit Santaputra <waritsan@gmail.com>
2018-02-18 14:26:47 +0700

    Add sprinkler example

---
+++ examples/SprinklerExample/Sprinkler.cpp
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+#include <WiFiManager.h>
+#include "ArduinoHomebridgeMqtt.h"
+#include "Switch.h"
+
+#define MQTT_SERVER IPAddress(192, 168, 1, 42)
+#define OUTPUT_PIN D1
+#define SERVICE_NAME "Sprinkler"
+#define SERVICE "Switch"
+
+WiFiManager wifiManager;
+ArduinoHomebridgeMqtt arduinoHomebridgeMqtt;
+Switch sprinkler(OUTPUT_PIN);
+
+void callback(String serviceName, String characteristic, bool value) {
+  if (serviceName == SERVICE_NAME) {
+    if (value) {
+      sprinkler.on();
+    } else {
+      sprinkler.off();
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  wifiManager.autoConnect();
+  arduinoHomebridgeMqtt.onSetValueFromHomebridge(callback);
+  arduinoHomebridgeMqtt.connect(MQTT_SERVER);
+  arduinoHomebridgeMqtt.addAccessory(SERVICE_NAME, SERVICE);
+  arduinoHomebridgeMqtt.getAccessory();
+}
+
+void loop() {
+}
