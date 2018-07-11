c223531cdcf48b0432049b4211f28ddd3e86e32e
Warit Santaputra <waritsan@gmail.com>
2018-07-11 19:04:38 +0700

    Update humidity sensor example and remove legacy libs.

--- examples/HumiditySensorExample/HumiditySensorExample.ino
+++ examples/HumiditySensorExample/HumiditySensorExample.ino
@@ -1,3 +1,55 @@
-// This file intentionally left empty
-// This file is used to provide compatibility with the Arduino IDE
-// See other tab for the code
+/*
+	HumiditySensorExample.ino
+
+	This humidity accessry reads the value sends it to Homebridge mqtt.
+
+	The circuit:
+	* Input
+        A0 - Soil moisture sensor
+
+	Created Jul 11, 2017
+	By Warit Santaputra
+	Modified Jul 11, 2018
+	By Warit Santaputra
+
+*/
+
+#include <Arduino.h>
+#include <WiFiManager.h>
+#include "ArduinoHomebridgeMqtt.h"
+
+const IPAddress MQTT_SERVER = IPAddress(192, 168, 1, 40);
+const int INPUT_PIN = A0;
+const String HUMIDITY_SENSOR_NAME = "HumiditySensor";
+const String HUMIDITY_SENSOR_SERVICE = "HumiditySensor";
+const String CURRENT_RELATIVE_HUMIDITY = "CurrentRelativeHumidity";
+
+long lastMsg = 0;
+WiFiManager wifiManager;
+ArduinoHomebridgeMqtt arduinoHomebridgeMqtt;
+
+int readHumidity(int inputPin) {
+  int value = analogRead(inputPin);
+  value = constrain(value, 567, 959);
+  return map(value, 567, 959, 100, 0);
+}
+
+void setup() {
+  Serial.begin(115200);
+  wifiManager.autoConnect();
+  arduinoHomebridgeMqtt.connect(MQTT_SERVER);
+  arduinoHomebridgeMqtt.addAccessory(HUMIDITY_SENSOR_NAME, HUMIDITY_SENSOR_SERVICE);
+  arduinoHomebridgeMqtt.getAccessory();
+}
+
+void loop() {
+  long now = millis();
+  if (now - lastMsg > 5000) {
+    lastMsg = now;
+    int currentHumidity = readHumidity(INPUT_PIN);
+    Serial.print("Current humidity ");
+    Serial.print(currentHumidity);
+    Serial.println("%");
+    arduinoHomebridgeMqtt.setValueToHomebridge(HUMIDITY_SENSOR_NAME, CURRENT_RELATIVE_HUMIDITY, currentHumidity);
+  }
+}
